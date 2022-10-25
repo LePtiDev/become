f72213a3b2aecfb917c8c5805763bb04d33bbe9c
LePtiDev <75quentinguerrier@gmail.com>
2022-10-26 00:43:07 +0200

    add router auth check

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
-import './assets/scss/init.scss'
 import App from './App.vue'
 import router from './router'
+
+import '@/assets/scss/init.scss'
 
 createApp(App).use(router).mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,36 @@
-import { createRouter, createMemoryHistory, createWebHistory } from 'vue-router'
 // Imports
+import { createRouter, createMemoryHistory, createWebHistory } from 'vue-router'
 import Home from '@/pages/Home.vue'
 import Signin from '@/pages/auth/Signin.vue'
 import Signup from '@/pages/auth/Signup.vue'
-import { app } from '@/helpers/firebase.ts'
+import { auth } from '@/helpers/firebase.ts'
+import { onAuthStateChanged } from 'firebase/auth'
 
 const isServer = typeof window === 'undefined'
 const history = isServer ? createMemoryHistory() : createWebHistory()
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/signin', component: Signin },
-  { path: '/signup', component: Signup },
+  { path: '/', name: 'home', component: Home, meta: { requiresAuth: true } },
+  { path: '/signin', name: 'signin', component: Signin },
+  { path: '/signup', name: 'signup', component: Signup },
 ]
 
 const router = createRouter({
   history,
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  onAuthStateChanged(auth, (user: any) => {
+    if (to.meta.requiresAuth && !user) {
+      next({
+        path: '/signin',
+        replace: true,
+      })
+    } else {
+      next()
+    }
+  })
 })
 
 export default router
