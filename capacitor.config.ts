2d9aa864c7e3914071515a49ed1f98968987bf8b
Quentin guerrier <75quentinguerrier@gmail.com>
2022-11-04 19:54:28 +0100

    add capacitor to the project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.become',
+  appName: 'become',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
